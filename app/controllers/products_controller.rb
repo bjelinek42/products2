6ca22006cf2a44871e68b1ac253b88691aded761
ben <bjelinek42@gmail.com>
2022-03-27 12:51:14 -0600

    index and create actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  def index
+    product = Product.all
+    render json: product
+  end
+
+  def create
+    product = Product.new(name: params[:name], description: params[:description], price: params[:price])
+    product.save
+    render json: product
+  end
 end
