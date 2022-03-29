6f0c3779dc092dd8049d84e673cb2cc416329827
ben <bjelinek42@gmail.com>
2022-03-29 09:09:48 -0600

    orders create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,14 @@ class OrdersController < ApplicationController
       carted_products.each do |carted_product|
         subtotal = carted_product.quantity * carted_product.product.price
       end
-      p subtotal
+      order = Order.new(user_id: current_user.id, subtotal: subtotal, tax: subtotal * 0.09, total: subtotal * 1.09)
+      order.save
+      carted_products.each do |carted_product|
+        carted_product[:status] = "purchased"
+        carted_product[:order_id] = order.id
+        carted_product.save
+      end
+      render json: order
     else
       render json: {message: "you must be logged in to order"}
     end
